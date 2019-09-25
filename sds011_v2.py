8b2194d13ad5e21b7bed060fb44f5a6d8f77f668
Trevor DiMartino <trevor.dimartino@cognizant.com>
2019-09-25 14:51:03 -0600

    Actually use logger object and update logging.

--- sds011_v2.py
+++ sds011_v2.py
@@ -40,11 +40,9 @@ class SDS011(object):
     SETTING_RESPONSE = b'\xc5'
     DATA_RESPONSE = b'\xc0'
 
-    logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
 
     def __init__(self, serial_port, baudrate=9600, timeout=2, use_query_mode=True, work_period=0):
-        """Initialise and open serial port.
-        """
         self.ser = serial.Serial(port=serial_port, baudrate=baudrate, timeout=timeout)
         self.ser.flush()
         self.default_timeout = timeout
@@ -53,14 +51,13 @@ class SDS011(object):
         self.reporting_mode = self.QUERY_MODE if use_query_mode else self.ACTIVE_MODE
 
     def _execute(self, cmd, id1=b'\xff', id2=b'\xff'):
-        """Writes a byte sequence to the serial.
-        """
+        """Writes a byte sequence to the serial."""
 
         cmd += id1 + id2
         checksum = bytes([sum(d for d in cmd) % 256])
         cmd_bytes = self.HEAD + self.CMD_ID + cmd + checksum + self.TAIL
 
-        logging.debug(f'Command: {cmd_bytes}')
+        self.logger.debug(f'Command: {cmd_bytes}')
         self.ser.write(cmd_bytes)
 
     def _get_reply(self, op_id=None, timeout=None):
@@ -70,14 +67,14 @@ class SDS011(object):
             self.ser.timeout = timeout
 
         raw = self.ser.read(size=10)
-        logging.debug(f'Reply: {raw}')
+        self.logger.debug(f'Reply: {raw}')
         if not raw:
-            logging.warning('Device did not respond; try sending a .wake()?')
+            self.logger.warning('Device did not respond; try sending a .wake()?')
             return raw
 
         data = raw[2:8]
         if (sum(d for d in data) & 255) != raw[8]:
-            raise IOError('Checksum did not match!')
+            raise IOError('Checksum invalid!')
 
         response_type = bytes([raw[1]])
         if response_type == self.DATA_RESPONSE:
@@ -85,7 +82,7 @@ class SDS011(object):
 
         if response_type == self.SETTING_RESPONSE:
             if not op_id:
-                raise UserWarning('Set the op_id when calling ._get_reply() for additional sanity checking.')
+                self.logger.warning('Set the op_id when calling ._get_reply() for additional sanity checking.')
             if op_id and bytes([raw[2]]) != op_id:
                 raise IOError('Response does not match operation!')
 
@@ -105,7 +102,7 @@ class SDS011(object):
             9 - Tail
         """
 
-        logging.debug(f'Processing frame: {raw}')
+        self.logger.debug(f'Processing frame: {raw}')
         pm25 = int.from_bytes(raw[2:4], byteorder='little') / 10.0
         pm10 = int.from_bytes(raw[4:6], byteorder='little') / 10.0
         return (pm25, pm10)
@@ -115,7 +112,7 @@ class SDS011(object):
         cmd = self.REPORTING_MODE_SETTING + self.READ_SETTING + b'\x00' * 11
         self._execute(cmd)
         reply = self._get_reply(op_id=self.REPORTING_MODE_SETTING)
-        logging.debug(f'Reporting mode read: {reply[4]}')
+        self.logger.debug(f'Reporting mode read: {reply[4]}')
         if bytes([reply[4]]) == self.QUERY_MODE:
             return 'sensor.QUERY_MODE'
         return 'sensor.ACTIVE_MODE'
@@ -128,11 +125,11 @@ class SDS011(object):
         cmd = self.REPORTING_MODE_SETTING + self.WRITE_SETTING + mode + b'\x00' * 10
         self._execute(cmd)
         reply = self._get_reply(op_id=self.REPORTING_MODE_SETTING)
-        logging.debug(f'Reporting mode set: {reply[4]}')
+        self.logger.debug(f'Reporting mode set: {reply[4]}')
         return reply[4]
 
     def query(self):
-        """Query the device and read the data.
+        """Query the sensor to read the data.
         @return: Air particulate density in micrograms per cubic meter.
         @rtype: tuple(float, float) -> (PM2.5, PM10)
         """
@@ -141,10 +138,6 @@ class SDS011(object):
         return self._get_reply()
 
     def _sleep(self, mode, write=True):
-        """Sleep/Wake up the sensor.
-        @param sleep: Whether the device should sleep or work.
-        @type sleep: bool
-        """
         cmd = self.SLEEP_SETTING + (self.WRITE_SETTING if write else self.READ_SETTING) + mode + b'\x00' * 10
         self._execute(cmd)
         return self._get_reply(op_id=self.SLEEP_SETTING)
@@ -159,7 +152,7 @@ class SDS011(object):
             attempts += 1
             awake = self._sleep(self.WORK_MODE)
         elif not awake:
-            logging.error('Device has not awoken after 5 attempts.')
+            self.logger.error('Device has not awoken after 5 attempts.')
             return
 
         return awake
@@ -169,33 +162,29 @@ class SDS011(object):
         cmd = self.WORK_PERIOD_SETTING + self.READ_SETTING + b'\x00' * 11
         self._execute(cmd)
         reply = self._get_reply(self.WORK_PERIOD_SETTING)
-        logging.debug(f'Work period read: {reply[4]}')
+        self.logger.debug(f'Work period read: {reply[4]}')
         return reply[4]
 
     @work_period.setter
     def work_period(self, work_time):
-        """Get work period command. Does not contain checksum and tail.
-        @rtype: list
-        """
         if work_time < 0:
             work_time = self.CONTINUOUS_MODE
         if work_time > 30:
-            logging.warn(f'Maximum work period is 30 minutes; received {work_time}, setting to 30.')
+            self.logger.warn(f'Maximum work period is 30 minutes; received {work_time}, setting to 30.')
             work_time = 30
 
         cmd = self.WORK_PERIOD_SETTING + self.WRITE_SETTING + bytes([work_time]) + b'\x00' * 10
         self._execute(cmd)
         reply = self._get_reply(self.WORK_PERIOD_SETTING)
-        logging.debug(f'Work period set: {reply[4]}')
+        self.logger.debug(f'Work period set: {reply[4]}')
         return reply[4]
 
     def read(self):
-        """Read sensor data.
-        @return: PM2.5 and PM10 concetration in micrograms per cude meter.
-        @rtype: tuple(float, float) - first is PM2.5.
-        """
+        """Wait for sensor to report a reading; a passive .query()"""
+
         if self.reporting_mode == 'sensor.QUERY_MODE':
             return self.query()
 
         timeout = self.work_period * 60 + 1
+        self.logger.debug(f'Waiting for a measurement for {timeout} seconds...')
         return self._get_reply(timeout=timeout)
